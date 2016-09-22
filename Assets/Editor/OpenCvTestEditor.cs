76c219bd00d5ae15f8b2ebddd2192ace24a56209
t79 <t79@users.noreply.github.com>
2016-09-22 12:50:10 +0200

    Adding OpenCV functionality

---
+++ Assets/Editor/OpenCvTestEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor (typeof (OpenCvTest))]
+public class OpenCvTestEditor : Editor {
+
+	public override void OnInspectorGUI() {
+
+		EditorUtility.SetDirty (target);
+		OpenCvTest oct;
+		oct = target as OpenCvTest;
+
+		if (oct == null) {
+			return;
+		}
+
+		base.DrawDefaultInspector ();
+
+		if (GUILayout.Button ("Run Script")) {
+			oct.RunScript ();
+		}
+
+	}
+}
---
+++ Assets/Script/OpenCvTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+using OpenCvSharp;
+
+public class OpenCvTest : MonoBehaviour {
+
+	public string imageFilePath = "";
+
+	public void RunScript() {
+
+		// Checking that that there exist a file of any type.
+		if (!File.Exists (imageFilePath)) {
+			Debug.Log ("Image File do not exist!");
+			//return;
+		}
+
+		Mat image = Cv2.ImRead (imageFilePath, ImreadModes.Color);
+
+		if (image.Empty()) {
+			Debug.Log ("No readable image file.");
+			return;
+		}
+
+		Cv2.NamedWindow ("Image", WindowMode.KeepRatio);
+		Cv2.ImShow ("Image", image);
+
+	}
+
+}
