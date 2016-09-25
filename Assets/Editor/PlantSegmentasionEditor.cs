c96b0349c3707746891486ab50c68991245aac41
t79 <t79@users.noreply.github.com>
2016-09-25 18:06:44 +0200

    Making a basic structure

--- Assets/Editor/PlantSegmentasionEditor.cs
+++ Assets/Editor/PlantSegmentasionEditor.cs
@@ -5,4 +5,21 @@ using System.Collections;
 [CustomEditor (typeof (PlantSegmentasion))]
 public class PlantSegmentasionEditor : Editor {
 
+	public override void OnInspectorGUI() {
+
+		EditorUtility.SetDirty (target);
+		PlantSegmentasion ps;
+		ps = target as PlantSegmentasion;
+
+		if (ps == null) {
+			return;
+		}
+
+		base.DrawDefaultInspector ();
+
+		if (GUILayout.Button ("Run Segmentasion")) {
+			ps.RunSegmentasion ();
+		}
+
+	}
 }
--- Assets/Script/PlantSegmentasion.cs
+++ Assets/Script/PlantSegmentasion.cs
@@ -1,15 +1,161 @@
 ﻿using UnityEngine;
 using System.Collections;
+using OpenCvSharp;
 
 public class PlantSegmentasion : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public string plantImageFilePath = "";
+	public string plantMaskFilePath = "";
+
+	public int reductionFactor = 4;
+	public int numRotationSteps = 24;
+
+	public string[] templetShapePath;
+	public int[] templetSizes;
+
+	public float matchingTreshold;
+
+	public bool generateNewTempletSizes = false;
+
+	// image matrixes
+	private Mat plantImageBGR;
+	private Mat plantImageGray;
+	private Mat plantMask;
+	private Mat plantEdges;
+
+	public void RunSegmentasion() {
+
+		if (!LoadImages()) {
+			Debug.Log ("Loading of images failed! Segmentasion aborted");
+		}
+
+		MakeGrayscaleImage ();
+
+		if (reductionFactor > 1) {
+			ReduceSegmentasionResolution ();
+		}
 	
+		MaskSegmentasionImage ();
+		CropSegmentasionImage ();
+
+		MakeEdgeMat ();
+
+		if (generateNewTempletSizes) {
+			GenerateNewTempletSizes (10);
+		}
+
+		int maxTempletSize = CalculateMaxTempletSize ();
+
+		TempletGenerater templetGenerator = new TempletGenerater ();
+
+		Mat matchinResultMat = Mat.Zeros(plantEdges.Size(), plantEdges.Type());
+
+		double minValue, maxValue;
+		Point minLoc, maxLoc;
+
+		foreach (string path in templetShapePath) {
+
+			if (!templetGenerator.LoadShape (path)) {
+				Debug.Log ("Could not generate templet from: " + path);
+				continue;
+			}
+
+			foreach (int templetSize in templetSizes) {
+
+				if (templetSize > maxTempletSize) {
+					continue;
+				}
+
+				templetGenerator.SetSize (templetSize);
+
+				for (int rotStep = 0; rotStep < numRotationSteps; ++rotStep) {
+
+					templetGenerator.SetRotasionStep (rotStep);
+
+					if (templetGenerator.toSmallMatchingArea ()) {
+						continue;
+					}
+
+					Mat matchingAreaMat = new Mat (PlantEdges, templetGenerator.GetMatchingRect ());
+					Mat templet = templetGenerator.GetTempletMat ();
+
+					OpenCvSharp.Rect matchingResultRect = new OpenCvSharp.Rect (new Point (0, 0), templetGenerator.GetMatchingResultSize ());
+					Mat matchingResultSubMat = new Mat (matchinResultMat, matchingResultRect);
+					matchingResultSubMat.SetTo (new Scalar (0));
+
+					Cv2.MatchTemplate (matchingAreaMat, templet, matchingResultSubMat, TemplateMatchModes.CCorrNormed);
+
+					Cv2.MinMaxLoc (matchingResultSubMat, out minValue, out maxValue, out minLoc, out maxLoc);
+
+					if (maxValue > matchingTreshold) {
+
+					}
+				}
+			}
+		}
+	}
+
+	private bool LoadImages() {
+		return false;
+	}
+
+	private void MakeGrayscaleImage() {
+
+	}
+
+	private void ReduceSegmentasionResolution() {
+
+	}
+
+	private void MaskSegmentasionImage() {
+
+	}
+
+	private void CropSegmentasionImage() {
+
+	}
+
+	private void MakeEdgeMat() {
+
+	}
+
+	private void GenerateNewTempletSizes(int numTemplets) {
+
+	}
+
+	private int CalculateMaxTempletSize() {
+
+	}
+}
+
+class TempletGenerater {
+
+	public bool LoadShape(string shapePath) {
+		return false;
+	}
+
+	public void SetSize(int size) {
+
+	}
+
+	public void SetRotasionStep(int rotStep) {
+
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	public OpenCvSharp.Rect GetMatchingRect() {
 	
 	}
+
+	public Mat GetTempletMat() {
+
+	}
+
+	public bool toSmallMatchingArea () {
+
+	}
+
+	public Size GetMatchingResultSize () {
+
+	}
+
 }
