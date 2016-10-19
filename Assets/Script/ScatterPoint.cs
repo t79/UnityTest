ede718f30f3880338ee1adb6813bbcea4418631f
t79 <t79@users.noreply.github.com>
2016-10-19 17:54:32 +0200

    Scatterplot point class and prefab

---
+++ Assets/Script/ScatterPoint.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScatterPoint : MonoBehaviour {
+
+	public Image point;
+
+	public void setPoint(int size, Color color) {
+		RectTransform rt = point.GetComponent<RectTransform> ();
+		rt.sizeDelta = new Vector2 (size, size);
+		point.color = color;
+	}
+}
